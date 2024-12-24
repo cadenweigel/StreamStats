f0eed0acce421422bfc3501ec796c1b3ff64f7ba
cadenweigel <157074200+cadenweigel@users.noreply.github.com>
2024-12-23 23:09:10 -0800

    added functions for printing 2 different ways

--- stream.py
+++ stream.py
@@ -1,4 +1,3 @@
-import json
 import sys
 from typing import List
 import parser
@@ -22,13 +21,20 @@ def main():
     bounds = parser.getStreamBounds(streams) #gives the dates that all streams occur within
     sys.setrecursionlimit(2000)
 
-    #get streams loaded into Artists and Songs
+    #get streams loaded into Artists and Songs, then sort
     parser.parseStreams(streams, Songs, Artists)
+    parser.sortSongs(Songs, 0, len(Songs)-1)
+    parser.sortArtists(Artists, 0, len(Artists)-1)
+
+    printToTerminal(streams, Artists, Songs)
+    printToFile(streams, Artists, Songs)
+
+
+def printToTerminal(streams, Artists, Songs):
+
     print("Total Streams: " + str(len(streams)))
     print("Total Artists: " + str(len(Artists)))
     print("Total Songs: " + str(len(Songs)))
-    parser.sortSongs(Songs, 0, len(Songs)-1)
-    parser.sortArtists(Artists, 0, len(Artists)-1)
 
     print("\n")
     for i in range(5):
@@ -37,6 +43,26 @@ def main():
     print("\n")
     for i in range(5):
         print(Songs[i].artist, Songs[i].title, Songs[i].streams, Songs[i].listenTime)
+
+def printToFile(streams, Artists, Songs):
+
+    f = open("stream_report.txt", "w")
+    f.write("Stream Data Report \n \n")
+
+    topArtists = ""
+    for i in range(5):
+        addString = Artists[i].name + " " + str(Artists[i].streams) + "\n"
+        topArtists += addString
+    f.write(topArtists + "\n")
+
+    topSongs = ""
+    for i in range(5):
+        addString = str(Songs[i].artist) + " " + Songs[i].title + " " + str(Songs[i].streams) + " " + str(Songs[i].listenTime) + "\n"
+        topSongs += addString
+    f.write(topSongs)
+
+    f.close()
+
 
 if __name__ == "__main__":
     main()
