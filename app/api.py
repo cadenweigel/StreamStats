a4b3a2b5d557ea71d36f1fe28aec865016e5bf5a
cadenweigel <157074200+cadenweigel@users.noreply.github.com>
2025-03-26 17:55:26 -0700

    api endpoints for individual artist/album/song pages

--- app/api.py
+++ app/api.py
@@ -4,23 +4,26 @@ from app import app
 
 DB_PATH = "database/streams.db"
 
+if __name__ == "__main__":
+    app.run(debug=True)
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-def get_top_artists():
+def get_top_artists(limit: int):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute("""
         SELECT artist_name, streams FROM Artists
         ORDER BY streams DESC
-        LIMIT 10;
-    """)
+        LIMIT ?;
+    """, (limit,))
     results = cursor.fetchall()
     conn.close()
     return [{"name": row[0], "streams": row[1]} for row in results]
 
-def get_top_albums():
+def get_top_albums(limit: int):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute("""
@@ -28,8 +31,8 @@ def get_top_albums():
         FROM Albums
         JOIN Artists ON Albums.artist_id = Artists.artist_id
         ORDER BY Albums.streams DESC
-        LIMIT 10;
-    """)
+        LIMIT ?;
+    """, (limit,))
     results = cursor.fetchall()
     conn.close()
     return [
@@ -37,7 +40,7 @@ def get_top_albums():
         for row in results
     ]
 
-def get_top_songs():
+def get_top_songs(limit: int):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute("""
@@ -45,8 +48,8 @@ def get_top_songs():
         FROM Songs
         JOIN Artists ON Songs.artist_id = Artists.artist_id
         ORDER BY Songs.streams DESC
-        LIMIT 10;
-    """)
+        LIMIT ?;
+    """, (limit,))
     results = cursor.fetchall()
     conn.close()
     return [
@@ -62,14 +65,32 @@ def get_data():
         "top_songs": get_top_songs()
     })
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 @app.route("/data", methods=["GET"])
 def data_page():
     return render_template(
         "data.html",
-        top_artists=get_top_artists(),
-        top_albums=get_top_albums(),
-        top_songs=get_top_songs()
+        top_artists=get_top_artists(10),
+        top_albums=get_top_albums(10),
+        top_songs=get_top_songs(10)
+    )
+
+@app.route("/data/artists", methods=["GET"])
+def data_page_artists():
+    return render_template(
+        "data_artists.html",
+        top_artists=get_top_artists(50)
+    )
+
+@app.route("/data/albums", methods=["GET"])
+def data_page_albums():
+    return render_template(
+        "data_albums.html",
+        top_albums=get_top_albums(50)
+    )
+
+@app.route("/data/songs", methods=["GET"])
+def data_page_songs():
+    return render_template(
+        "data_songs.html",
+        top_songs=get_top_songs(50)
     )
