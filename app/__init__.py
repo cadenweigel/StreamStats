a3e41c14110d759e9c174cd263c911af0081d4d7
cadenweigel <157074200+cadenweigel@users.noreply.github.com>
2025-04-06 20:22:59 -0700

    updated data pages to show listen time

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+from datetime import timedelta
+
+def format_milliseconds(ms):
+    seconds = int(ms / 1000)
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    secs = seconds % 60
+    return f"{hours:02}:{minutes:02}:{secs:02}"
+
+app.jinja_env.filters['format_time'] = format_milliseconds
